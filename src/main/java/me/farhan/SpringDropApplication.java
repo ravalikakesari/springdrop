2ca795af71020a45fbc329ed59d791d00b886464
Farhan Faruque <russel.cse05@gmail.com>
2017-01-06 11:49:46 +0600

    intial

---
+++ src/main/java/me/farhan/SpringDropApplication.java
@@ -0,0 +1,12 @@
+package me.farhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDropApplication.class, args);
+	}
+}
