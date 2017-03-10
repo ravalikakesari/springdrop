bf0545dca87659b00e6e659af09142a6e8c47a12
Farhan Faruque <russel.cse05@gmail.com>
2017-03-10 19:39:09 +0600

    user registration: add user dto

--- src/main/java/me/farhan/controllers/UsersController.java
+++ src/main/java/me/farhan/controllers/UsersController.java
@@ -1,15 +1,20 @@
 package me.farhan.controllers;
 
+import me.farhan.dto.UserDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(path = "/users")
+@RequestMapping(value = "users")
 public class UsersController {
 
-    @RequestMapping(path = "/register")
-    public String register(Model model){
+    @RequestMapping(path = "/register",method = RequestMethod.GET)
+    public String showRegistrationFrom(Model model){
+        UserDto userDto = new UserDto();
+        model.addAttribute("user",userDto);
+
         return "user/register";
     }
 }
